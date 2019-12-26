c9697d643228311ff5cf1d7b5f111703e77be84d
GunnlaugurG <34029342+GunnlaugurG@users.noreply.github.com>
2019-12-26 15:58:01 +0000

    added nods server for producion

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,16 @@ module.exports = () => {
                     {
                         loader: "css-loader"
                     }]
+                },
+                {
+                    test: /\.(png|jp(e*)g|svg)$/,  
+                    use: [{
+                        loader: 'url-loader',
+                        options: { 
+                            limit: 8000, // Convert images < 8kb to base64 strings
+                            name: 'images/[hash]-[name].[ext]'
+                        } 
+                    }]
                 }
             ]
         },
